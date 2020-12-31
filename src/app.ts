0809f01988e49afbe61e4d8bd3ecbe0013dcb8d8
evaou <speeder528@gmail.com>
2020-12-31 18:11:59 +0800

    update property name

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,28 @@
 import * as fs from "fs";
 class App {
-    inputs: string[];
+    private commands: string[];
 
     constructor() {
-        this.inputs = [];
+        this.commands = [];
     }
 
     readFile(filePath: string): boolean {
         let data: Buffer = fs.readFileSync(filePath);
-        this.inputs = data.toString().split("\n");
+        this.commands = data.toString().split("\n");
 
         return true;
     }
 
     runCommands(): void {
-        if (this.inputs.length === 0) {
+        if (this.commands.length === 0) {
             return;
         }
 
         let command: string;
         let action: string;
 
-        for (let i = 0; i < this.inputs.length; i++) {
-            command = this.inputs[i];
+        for (let i = 0; i < this.commands.length; i++) {
+            command = this.commands[i];
             if (command.length === 0) {
                 continue;
             }
