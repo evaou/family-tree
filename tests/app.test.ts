61b3c3c276431c1d5b562aa277a6777d1a985c47
evaou <speeder528@gmail.com>
2021-01-01 22:33:56 +0800

    add test for ADD_KING_QUEEN

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,6 +2,33 @@ import { expect } from "chai";
 import * as sinon from "sinon";
 import app from "../src/app";
 
+describe("add king queen", function () {
+    let filePath: string = "./tests/input/add-king-queen.txt";
+    let exampleOutput: string[] = [
+        "KING_QUEEN_ADDED"
+    ];
+
+    it("readFile", function () {
+        let result = app.readFile(filePath);
+        expect(result).equal(true);
+    });
+
+    it("runCommands", function () {
+        const sinonLog = sinon.spy(console, "log");
+        let callArray;
+        let output: string;
+
+        app.runCommands();
+
+        callArray = sinonLog.getCalls();
+
+        for (let i = 0; i < exampleOutput.length; i++) {
+            output = exampleOutput[i];
+            expect(callArray[i].firstArg).equal(output);
+        }
+    });
+});
+
 describe("shippit example", function () {
     let filePath: string = "./tests/input/shippit-example.txt";
     let exampleOutput: string[] = [
