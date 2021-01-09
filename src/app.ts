0fb0e3222013d99d89dc71421e51a572760c953a
evaou <speeder528@gmail.com>
2021-01-09 10:09:14 +0800

    refactor cli entry to another file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { CommandUtil } from "./commandUtil";
 import { FamilyTree } from "./familyTree";
 import { FamilyTreeBuilder } from "./familyTreeBuilder";
+
 export class App {
     tree: FamilyTree;
     commandUtil: CommandUtil = new CommandUtil();
@@ -24,28 +25,3 @@ export class App {
         }
     }
 }
-/*
-let app = new App();
-let args = process.argv.slice(2);
-let testFilePath = args[0];
-let regStr: RegExp = /^(\.\/)?input\//;
-let commands: string[] = [];
-
-if (args.length === 1) {
-    commands = app.commandUtil.readFile(testFilePath);
-
-    if (commands.length > 0) {
-        app.runCommands(commands);
-    } else {
-        if (testFilePath.match(regStr)) {
-            testFilePath = testFilePath.replace(regStr, "");
-            console.log(
-                "Below file doesn't exist under mounted host directory\n" +
-                    testFilePath
-            );
-        } else {
-            console.log("Below file doesn't exist\n" + testFilePath);
-        }
-    }
-}
-*/
