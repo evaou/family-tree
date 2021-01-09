faa5eae69209b7a00abddb3062bb601408bbdd14
evaou <speeder528@gmail.com>
2021-01-09 08:54:05 +0800

    add tree file path

--- src/geektrust.ts
+++ src/geektrust.ts
@@ -1,6 +1,8 @@
 import { App } from "./app";
 
-let app = new App();
+const treeFilePath: string = "./res/geektrust-tree.txt";
+
+let app = new App(treeFilePath);
 let args = process.argv.slice(2);
 let testFilePath = args[0];
 let commands: string[] = [];
